c59bd4e8ea006ee764506a988d7607f57d981336
Greiveldinger Victor <victorgreiveldinger@gmail.com>
2023-02-02 07:53:51 +0100

    Ajout Header, Footer, nav

--- dactapp/src/index.js
+++ dactapp/src/index.js
@@ -1,7 +1,14 @@
+// == Imports globaux
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Components/App';
 import { BrowserRouter } from 'react-router-dom';
+
+// == Import component
+import App from './Components/App';
+
+// == Import propre au fichier
+import './reset.css';
+import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
